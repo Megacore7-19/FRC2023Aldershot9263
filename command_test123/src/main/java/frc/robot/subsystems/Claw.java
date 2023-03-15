bd4007f769a8bc3f795bc28f14bf01abd2ff9a90
Megacore7-19 <52516060+Megacore7-19@users.noreply.github.com>
2023-03-15 19:16:52 -0400

    Tried to add CAN

--- command_test123/src/main/java/frc/robot/subsystems/Claw.java
+++ command_test123/src/main/java/frc/robot/subsystems/Claw.java
@@ -1,49 +1,58 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
+import edu.wpi.first.wpilibj.CAN;
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj.motorcontrol.Victor;
+import edu.wpi.first.wpilibj.motorcontrol.Talon;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;;
+
 /**
  * The claw subsystem is a simple system with a motor for opening and closing. If using stronger
  * motors, you should probably use a sensor so that the motors don't stall.
  */
 public class Claw extends SubsystemBase {
-  private final Victor m_motorRight = new Victor(7);
-  private final Victor m_motorLeft = new Victor(8);
+  private final TalonSRX m_motorRight = new TalonSRX(0);
+  private final TalonSRX m_motorLeft = new TalonSRX(0);
+
+  //private final CAN m_motorLeft = new CAN(0);
+  
   private final DigitalInput m_contact = new DigitalInput(7);
   private final double m_speed = 0.085;
 
   /** Create a new claw subsystem. */
   public Claw() {
     // Let's name everything on the LiveWindow
-    addChild("MotorLeft", m_motorRight);
-    addChild("MotorRight", m_motorRight);
+    // addChild("MotorLeft", m_motorRight);
+    // addChild("MotorRight", m_motorRight);
   }
 
   public void log() {
     //SmartDashboard.putData("Claw switch", m_contact);
-    SmartDashboard.putNumber("Claw - Left", m_motorLeft.get());
-    SmartDashboard.putNumber("Claw - Right", m_motorRight.get());
+    SmartDashboard.putNumber("Claw - Left", m_motorLeft.getMotorOutputPercent());
+    SmartDashboard.putNumber("Claw - Right", m_motorRight.getMotorOutputPercent());
   }
 
   /** Set the claw motor to move in the open direction. */
   public void open() {
-    m_motorLeft.set(-m_speed);
-    m_motorRight.set(m_speed);
+    m_motorLeft.set(ControlMode.PercentOutput, -m_speed);
+    m_motorRight.set(ControlMode.PercentOutput, m_speed);
   }
 
   /** Set the claw motor to move in the close direction. */
   public void close() {
-    m_motorLeft.set(m_speed);
-    m_motorRight.set(-m_speed);
+    m_motorLeft.set(ControlMode.PercentOutput, m_speed);
+    m_motorRight.set(ControlMode.PercentOutput, -m_speed);
   }
 
   /** Stops the claw motor from moving. */
   public void stop() {
-    m_motorLeft.set(0);
-    m_motorRight.set(0);
+    m_motorLeft.set(ControlMode.PercentOutput, 0);
+    m_motorRight.set(ControlMode.PercentOutput, 0);
   }
 
   /** Return true when the robot is grabbing an object hard enough to trigger the limit switch. */
